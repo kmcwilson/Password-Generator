bf1d68721c3571c06c04755d4ad83909104b96f3
Katie Wilson <kmcwilson4@gmail.com>
2022-08-30 18:25:00 -0400

    added variables

--- script.js
+++ script.js
@@ -1,37 +1,45 @@
 // Assignment Code
-var generateBtn = document.getElementById("#generate");
+var generateBtn = document.querySelector("#generate");
+
+let specialCharacters = ["!", "@", "#", '$', '%', '^', '&', '*', '(', ')', '+', '-'];
+let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowerCase= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+let numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let chosenOnes = []
 
 // Write password to the #password input
-let passwordPrompt = prompt ("Choose how many characters you would like to include in your password");
-alert (passwordPrompt);
-if (passwordPrompt < 8)
-   {
-    window.alert("Password must at least 8 characters");
-    alert (passwordPrompt);
-  } else if (passwordPrompt > 128) {
+function generatePassword () {
+  let passwordPrompt = prompt ("Choose how many characters you would like to include in your password");
+  alert (passwordPrompt);
+  if (passwordPrompt < 8)
+     {
+      window.alert("Password must at least 8 characters");
+      alert (passwordPrompt);
+    } else if (passwordPrompt > 128) {
 
-    window.alert("Password must be no more than 128 characters");
-    alert (passwordPrompt);
+      window.alert("Password must be no more than 128 characters");
+      alert (passwordPrompt);
 
-  } else if (passwordPrompt > 8, passwordPrompt < 128) {
-    window.alert ("Click OK to include special characters in your password");
+    } else if (passwordPrompt > 8, passwordPrompt < 128) {
+      window.alert ("Click OK to include special characters in your password");
+    } 
+  
   }
-
+  
 
 function writePassword() {
-  var password = ;
-  var passwordText = document.getElementById("#password");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
+generateBtn.addEventListener("click", writePassword);
 
-
-generatePassword()
-writePassword()
+writePassword ()
 
 
 //function generatePassword (){
@@ -45,4 +53,3 @@ writePassword()
   
  // document.getElementById ("password").value=password;
   //
-}
