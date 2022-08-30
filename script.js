13b40dba3d85f9431c838898322fcbed6d80fe05
Katie Wilson <kmcwilson4@gmail.com>
2022-08-30 11:23:58 -0400

    added to CSS and attempting to create a random password generator

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 var generateBtn = document.getElementById("#generate");
 
 // Write password to the #password input
+function generatePassword (){
+  var characters= "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 12;
+  var password = "";
+
+  for (var i = 0; i <= passwordLength; i++){
+    var randomPassword= Math.floor(Math.random() * characters.length);
+    password += characters.substring (randomPassword, randomPassword +1);
+  }
+  document.getElementById ("password").value=password;
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.getElementById("#password");
@@ -13,6 +25,4 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-function genPassword (){
-  var characters= "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-}
+
