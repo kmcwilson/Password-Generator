f48b6770304bca415b668757a090588ecee09bc2
Katie Wilson <kmcwilson4@gmail.com>
2022-08-30 19:09:45 -0400

    added information for writing the password

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 let specialCharacters = ["!", "@", "#", '$', '%', '^', '&', '*', '(', ')', '+', '-'];
@@ -8,15 +7,16 @@ let numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 let chosenOnes = []
 
 // Write password to the #password input
-
 function generatePassword () {
-  let passwordLength = parseInt(prompt ("Choose how many characters you would like to include in your password"));
+  let  passwordLength = parseInt(prompt ("Choose how many characters you would like to include in your password"));
   
   while (passwordLength <= 8 || passwordLength >= 128)
   {
     window.alert("Password must contain at least 8 characters and less than 128 characters");
     passwordLength = parseInt(prompt ("Choose how many characters you would like to include in your password"));
-  }
+    //return passwordPrompt;  
+ }
+  
   let specialCharactersChoice = window.confirm ('Click OK to include special characters in your password');
   let uppercaseChoice = window.confirm ('Click OK to include Uppercase letters');
   let lowerCaseChoice= window.confirm ('Click OK to include lowercase letters');
@@ -35,20 +35,23 @@ function generatePassword () {
   if (numbersChoice) {
     chosenOnes.concat(numbers)
   }
-for 
 }
+  
+    
+  
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
 }
+
+  
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+generatePassword()
 writePassword ()
-
-
-
