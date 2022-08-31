a78e3bffb01adf732d07709e881e22e7c2108e37
Katie Wilson <kmcwilson4@gmail.com>
2022-08-31 18:08:04 -0400

    added a correct for loop and changed concat to push and join

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const generateBtn = document.querySelector("#generate");
 let specialCharacters = ["!", "@", "#", '$', '%', '^', '&', '*', '(', ')', '+', '-'];
 let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let lowerCase= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let numbers = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let numbers = [ '1','2', '3', '4', '5', '6', '7', '8', '9', '0'];
 let chosenOnes = []
 
 // Write password to the #password input
 function generatePassword () {
   let  passwordLength = parseInt(prompt ("Choose how many characters you would like to include in your password"));
+ 
   
-  while (passwordLength < 8 || passwordLength > 128)
+  while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength))
   {
     window.alert("Password must contain at least 8 characters and less than 128 characters");
     passwordLength = parseInt(prompt ("Choose how many characters you would like to include in your password"));
@@ -23,24 +24,31 @@ function generatePassword () {
   let numbersChoice=window.confirm ('Click OK to include numbers in your password');
 
   if (specialCharactersChoice) {
-      chosenOnes.concat(specialCharacters) }
+      chosenOnes.push(specialCharacters.join('')) }
 
  if (uppercaseChoice) { 
-      chosenOnes.concat(upperCase) }
+      chosenOnes.push(upperCase.join('')) }
 
   if (lowerCaseChoice){
-     chosenOnes.concat(lowerCase)
+     chosenOnes.push(lowerCase.join(''))
 
   }
   if (numbersChoice) {
-    chosenOnes.concat(numbers)
+    chosenOnes.push(numbers.join(''))
   }
-}
+chosenOnes=chosenOnes.join('').split('')
 
-  //function finalPassword() {
-  //for (var i=0; i<= passwordLength.length; i++){
-  //chosenOnes[i].textContent= "";}
-  
+console.log(chosenOnes)
+
+ let finalPassword = []
+
+  for (var i=0; i < passwordLength; i++){
+    let randomIndex= Math.floor (Math.random() * chosenOnes.length);
+    finalPassword.push (chosenOnes[randomIndex]);
+    }
+    return finalPassword.join('');
+ }
+
 
 function writePassword() {
   var password = generatePassword();
@@ -50,5 +58,5 @@ function writePassword() {
 
   
 // Add event listener to generate button
-generateBtn.addEventListener("click", generatePassword);
- 
+generateBtn.addEventListener("click", writePassword);
+
